264f07bd54dd1efe3369258d07c1139a4fe9d64b
marco <marco@unistra.fr>
2025-04-04 11:07:35 +0200

    🔧 Add sentry config

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import router from './router'
 // @ts-ignore: No types for this package
 import VueMatomo from 'vue-matomo'
 
+import * as Sentry from '@sentry/vue'
+
 const app = createApp(App)
 
 if (import.meta.env.VITE_ENV !== 'prod') {
@@ -17,6 +19,23 @@ if (import.meta.env.VITE_ENV !== 'prod') {
         host: import.meta.env.VITE_MATOMO_SERVER,
         siteId: import.meta.env.VITE_MATOMO_SITE_ID,
         debug: import.meta.env.VITE_MATOMO_DEBUG || false,
+    })
+}
+
+if (import.meta.env.MODE !== 'development') {
+    Sentry.init({
+        app,
+        dsn: import.meta.env.VITE_SENTRY_DSN,
+        integrations: [Sentry.browserTracingIntegration({ router }), Sentry.replayIntegration()],
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for tracing.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+
+        // Capture Replay for 10% of all sessions,
+        // plus for 100% of sessions with an error
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
     })
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '#': fileURLToPath(new URL('./types', import.meta.url)),
+            '~': fileURLToPath(new URL('./tests', import.meta.url)),
+        },
     },
-  },
+    build: {
+        sourcemap: true,
+    },
 })
