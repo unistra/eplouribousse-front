f302fad0e8a2f0d3f3660736155845346cce27b2
marco <marco@unistra.fr>
2025-04-04 11:36:31 +0200

    🔧 Add quasar config

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,17 @@ import VueMatomo from 'vue-matomo'
 
 import * as Sentry from '@sentry/vue'
 
+// Import Quasar plugins
+import { Loading, Notify, Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/fr'
+import quasarIconSet from 'quasar/icon-set/mdi-v7'
+
+// Import icon libraries
+import '@quasar/extras/mdi-v7/mdi-v7.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 const app = createApp(App)
 
 if (import.meta.env.VITE_ENV !== 'prod') {
@@ -39,6 +50,26 @@ if (import.meta.env.MODE !== 'development') {
     })
 }
 
+app.use(Quasar, {
+    plugins: {
+        Notify,
+        Loading,
+    }, // import Quasar plugins and add here
+    lang: quasarLang,
+    iconSet: quasarIconSet,
+    /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
 app.use(createPinia())
 app.use(router)
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+        vue({
+            template: { transformAssetUrls },
+        }),
+        vueDevTools(),
+        quasar({
+            autoImportComponentCase: 'pascal',
+            sassVariables: fileURLToPath(new URL('./src/assets/quasar-variables.sass', import.meta.url)),
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
