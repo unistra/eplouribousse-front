fa3b6704ac7f5885941b8a43f53a8621f01555fa
mhmae <mhmae@unsitra.fr>
2025-04-16 10:19:55 +0200

    🔧 Add VITE_I18N_LOCALE to environment type definitions

--- env.d.ts
+++ env.d.ts
@@ -5,5 +5,6 @@ interface ImportMetaEnv {
     readonly VITE_MATOMO_SERVER: string
     readonly VITE_MATOMO_SITE_ID: string
     readonly VITE_MATOMO_DEBUG: string
+    readonly VITE_I18N_LOCALE: string
     readonly VITE_APP_BASE_URL: string
 }
