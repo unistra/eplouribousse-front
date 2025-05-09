26d4aaec48317182b1e0452ed8d1fbe81d0a0daa
mhmae <mhmae@unsitra.fr>
2025-05-09 14:44:37 +0200

    ♻️✨ Refactor ChangePasswordForm to use PasswordField component and improve password validation logic

--- src/components/forms/changePassword/ChangePasswordForm.vue
+++ src/components/forms/changePassword/ChangePasswordForm.vue
@@ -1,94 +1,36 @@
 <script setup lang="ts">
-import { useFormUtils } from '@/composables/useFormUtils.ts'
 import { useChangePasswordForm } from './useChangePasswordForm.ts'
 import { useI18n } from 'vue-i18n'
-import LinearProgress from '@/components/utils/linearProgress/LinearProgress.vue'
+import PasswordField from '@/components/utils/form/PasswordField.vue'
 const { t } = useI18n()
 const {
     changePassword,
     oldPassword,
     newPassword,
-    passwordStrength,
     confirmPassword,
     isLoading,
-    isNewPasswordValid,
-    doPasswordsMatch,
+    isPasswordStrongEnough,
+    arePasswordsMatching,
 } = useChangePasswordForm()
-const { icon, passwordVisibility, passwordVisibilityLabel, updatePasswordVisibility } = useFormUtils()
 </script>
 
 <template>
     <QForm @submit.prevent="changePassword">
-        <QInput
+        <PasswordField
             v-model="oldPassword"
             :label="t('forms.password.oldPassword')"
-            :type="passwordVisibility"
-            required
-            autofocus
-            ><template #append>
-                <QBtn
-                    flat
-                    dense
-                    rounded
-                    data-testid="visibility-button"
-                    :icon="icon"
-                    @click="updatePasswordVisibility"
-                >
-                    <QTooltip>
-                        {{ t('forms.login.' + passwordVisibilityLabel) }}
-                    </QTooltip>
-                </QBtn>
-            </template>
-        </QInput>
-        <QInput
+        />
+        <PasswordField
             v-model="newPassword"
             :label="t('forms.password.newPassword')"
-            :type="passwordVisibility"
-            data-testid="new-password"
-            required
-            :rules="[() => isNewPasswordValid || t('forms.password.passwordRequirements')]"
-            ><template #append>
-                <QBtn
-                    flat
-                    dense
-                    rounded
-                    data-testid="visibility-button"
-                    :icon="icon"
-                    @click="updatePasswordVisibility"
-                >
-                    <QTooltip>
-                        {{ t('forms.login.' + passwordVisibilityLabel) }}
-                    </QTooltip>
-                </QBtn>
-            </template></QInput
-        >
-        <LinearProgress
-            :password-strength="passwordStrength"
-            v-if="newPassword.length > 0"
-            data-testid="progress"
+            :linear-progress="true"
+            :rules="[() => isPasswordStrongEnough || t('forms.password.passwordRequirements')]"
         />
-
-        <QInput
+        <PasswordField
             v-model="confirmPassword"
             :label="t('forms.password.confirmPassword')"
-            :type="passwordVisibility"
-            required
-            :rules="[() => doPasswordsMatch || t('forms.password.passwordsDoNotMatch')]"
-            ><template #append>
-                <QBtn
-                    flat
-                    dense
-                    rounded
-                    data-testid="visibility-button"
-                    :icon="icon"
-                    @click="updatePasswordVisibility"
-                >
-                    <QTooltip>
-                        {{ t('forms.login.' + passwordVisibilityLabel) }}
-                    </QTooltip>
-                </QBtn>
-            </template></QInput
-        >
+            :rules="[() => arePasswordsMatching || t('forms.password.passwordsDoNotMatch')]"
+        />
 
         <div class="password-requirements">
             <p>{{ t('forms.password.passwordMustContain') }}:</p>
@@ -113,6 +55,10 @@ const { icon, passwordVisibility, passwordVisibilityLabel, updatePasswordVisibil
 </template>
 
 <style scoped>
+form {
+    max-width: 256px;
+    width: 100%;
+}
 .submit-btn {
     margin-top: 16px;
 }
