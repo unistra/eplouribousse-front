6b8fe4e62933bf06e555183c000a21279108bc59
mhmae <mhmae@unsitra.fr>
2025-05-09 07:58:27 +0200

    🔀 Trying to git pull rebase from develop but resolve conflicts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -69,6 +69,16 @@ const routes: RouteRecordRaw[] = [
             title: 'Handshake',
         },
     },
+    {
+        path: '/invite',
+        name: 'inviteNewUser',
+        component: () => import('@/views/InviteView.vue'),
+        meta: {
+            title: 'Invite new user',
+            hiddenInNav: true,
+            unistraCasAuthentication: true,
+        },
+    },
 ]
 
 export default routes
