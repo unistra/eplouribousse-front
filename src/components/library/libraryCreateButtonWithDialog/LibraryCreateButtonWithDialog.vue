48f3ef2ffe34201f45674cee86e2d81286cbdfea
mhmae <mhmae@unsitra.fr>
2025-05-27 08:39:47 +0200

    ✨ Enhance library creation dialog to support editing and improve error handling

--- src/components/library/libraryCreateButtonWithDialog/LibraryCreateButtonWithDialog.vue
+++ src/components/library/libraryCreateButtonWithDialog/LibraryCreateButtonWithDialog.vue
@@ -1,68 +1,89 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useLibraryCreateButtonWithDialog } from '@/components/library/libraryCreateButtonWithDialog/useLibraryCreateButtonWithDialog.ts'
+import type { Library } from '#/library.ts'
+import { onMounted } from 'vue'
 
-defineProps<{
+const props = defineProps<{
     buttonLabel?: string
     buttonIcon?: string
+    libraryToEdit?: Library
 }>()
 
 const { t } = useI18n()
 
-const { dialog, openDialog, library, createLibrary, nameError, aliasError, codeError } =
+const { dialog, openDialog, library, createLibrary, nameError, aliasError, codeError, updateLibrary } =
     useLibraryCreateButtonWithDialog()
 
+const isToEdit = !!props.libraryToEdit
+
 const emit = defineEmits<{
-    created: [value: boolean]
+    submitted: []
 }>()
 
-const createLibraryAndEmit = async () => {
+const onSubmit = async () => {
     try {
-        await createLibrary()
-        emit('created', true)
+        isToEdit ? await updateLibrary() : await createLibrary()
+        emit('submitted')
     } catch {
-        console.error('Failed to create library')
+        console.error('Failed to create or update library')
     }
 }
+onMounted(() => {
+    if (props.libraryToEdit) {
+        library.id = props.libraryToEdit.id
+        library.name = props.libraryToEdit.name
+        library.alias = props.libraryToEdit.alias
+        library.code = props.libraryToEdit.code
+    }
+})
 </script>
 
 <template>
-    <QBtn
-        :icon="buttonIcon || 'mdi-plus'"
-        :label="buttonLabel || t('libraries.add.title')"
-        @click="openDialog"
-    />
+    <slot
+        name="button"
+        :openDialog="openDialog"
+    >
+        <QBtn
+            :icon="buttonIcon || 'mdi-plus'"
+            :label="buttonLabel || t('libraries.form.add')"
+            @click="openDialog"
+        />
+    </slot>
     <QDialog
         v-model="dialog"
         persistent
     >
         <QCard>
             <QCardSection>
-                <div class="text-h6">{{ t('libraries.add.title') }}</div>
+                <div class="text-h6">
+                    {{ isToEdit ? t('libraries.form.edit') : t('libraries.form.add') }}
+                    {{ t('libraries.form.aLibrary') }}
+                </div>
             </QCardSection>
 
-            <QForm @submit.prevent="createLibraryAndEmit">
+            <QForm @submit.prevent="onSubmit">
                 <QCardSection>
                     <QInput
                         v-model="library.name"
                         autofocus
                         :error="!!nameError"
                         :error-message="nameError"
-                        :label="t('libraries.add.fields.name')"
+                        :label="t('libraries.form.fields.name')"
                         :rules="[(val) => !!val || t('forms.validation.fieldIsRequired')]"
                     />
                     <QInput
                         v-model="library.alias"
                         :error="!!aliasError"
                         :error-message="aliasError"
-                        :label="t('libraries.add.fields.alias')"
+                        :label="t('libraries.form.fields.alias')"
                         :rules="[(val) => !!val || t('forms.validation.fieldIsRequired')]"
                     />
                     <QInput
                         v-model="library.code"
                         :error="!!codeError"
                         :error-message="codeError"
-                        :label="t('libraries.add.fields.code')"
+                        :label="t('libraries.form.fields.code')"
                         :rules="[(val) => !!val || t('forms.validation.fieldIsRequired')]"
                     />
                 </QCardSection>
@@ -74,11 +95,11 @@ const createLibraryAndEmit = async () => {
                     <QBtn
                         v-close-popup
                         flat
-                        :label="t('libraries.add.cancel')"
+                        :label="t('libraries.form.cancel')"
                     />
                     <QBtn
                         flat
-                        :label="t('libraries.add.submit')"
+                        :label="isToEdit ? t('libraries.form.edit') : t('libraries.form.add')"
                         type="submit"
                     />
                 </QCardActions>
--- src/components/library/libraryCreateButtonWithDialog/useLibraryCreateButtonWithDialog.ts
+++ src/components/library/libraryCreateButtonWithDialog/useLibraryCreateButtonWithDialog.ts
@@ -4,6 +4,7 @@ import { axiosI } from '@/plugins/axios/axios.ts'
 import { useComposableQuasar } from '@/composables/useComposableQuasar.ts'
 import { useI18n } from 'vue-i18n'
 import { AxiosError } from 'axios'
+import { useLibraryStore } from '@/stores/libraryStore.ts'
 
 export const useLibraryCreateButtonWithDialog = () => {
     const { notify } = useComposableQuasar()
@@ -22,14 +23,50 @@ export const useLibraryCreateButtonWithDialog = () => {
     const aliasError = ref<string | undefined>(undefined)
     const codeError = ref<string | undefined>(undefined)
 
-    // Function to reset errors
     const resetErrors = () => {
         nameError.value = undefined
         aliasError.value = undefined
         codeError.value = undefined
     }
 
-    const createLibrary = async () => {
+    const handleError = (error: unknown) => {
+        if (error instanceof AxiosError && error.response?.data) {
+            const errors = error.response.data
+
+            const errorFields = {
+                name: nameError,
+                alias: aliasError,
+                code: codeError,
+            }
+
+            for (const [field, errorRef] of Object.entries(errorFields)) {
+                if (errors[field]) {
+                    errorRef.value = Array.isArray(errors[field]) ? errors[field][0] : errors[field]
+                }
+            }
+        } else {
+            notify({
+                type: 'negative',
+                message: t('errors.unknown'),
+            })
+        }
+    }
+
+    const updateLibraryStore = async () => {
+        const libraryStore = useLibraryStore()
+        try {
+            await libraryStore.fetchLibraries({
+                page: 1,
+                pageSize: 10,
+                sortBy: 'name',
+                descending: false,
+            })
+        } catch (error) {
+            handleError(error)
+        }
+    }
+
+    const createLibrary = async (updateStore: boolean = false) => {
         resetErrors()
         try {
             await axiosI.post<Library>('/libraries/', {
@@ -43,27 +80,52 @@ export const useLibraryCreateButtonWithDialog = () => {
             library.code = ''
 
             dialog.value = false
+
+            if (updateStore) await updateLibraryStore()
         } catch (error: unknown) {
-            if (error instanceof AxiosError && error.response?.data) {
-                const errors = error.response.data
+            handleError(error)
+        }
+    }
 
-                const errorFields = {
-                    name: nameError,
-                    alias: aliasError,
-                    code: codeError,
-                }
+    const updateLibrary = async (updateStore: boolean = false) => {
+        resetErrors()
 
-                for (const [field, errorRef] of Object.entries(errorFields)) {
-                    if (errors[field]) {
-                        errorRef.value = Array.isArray(errors[field]) ? errors[field][0] : errors[field]
-                    }
-                }
-            } else {
-                notify({
-                    type: 'negative',
-                    message: t('errors.unknown'),
-                })
-            }
+        const libraryStore = useLibraryStore()
+        const originalLibrary = libraryStore.libraries.results.find((lib) => lib.id === library.id)
+
+        if (!originalLibrary) {
+            notify({
+                type: 'negative',
+                message: t('libraries.errors.notFound'),
+            })
+            return
+        }
+
+        const hasChanges =
+            originalLibrary.name !== library.name ||
+            originalLibrary.alias !== library.alias ||
+            originalLibrary.code !== library.code
+
+        if (!hasChanges) {
+            dialog.value = false
+            return
+        }
+
+        try {
+            await axiosI.patch<Library>(`/libraries/${library.id}/`, {
+                name: library.name,
+                alias: library.alias,
+                code: library.code,
+            })
+
+            dialog.value = false
+            if (updateStore) await updateLibraryStore()
+
+            library.name = ''
+            library.alias = ''
+            library.code = ''
+        } catch (error: unknown) {
+            handleError(error)
         }
     }
 
@@ -75,5 +137,6 @@ export const useLibraryCreateButtonWithDialog = () => {
         nameError,
         aliasError,
         codeError,
+        updateLibrary,
     }
 }
