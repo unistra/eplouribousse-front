50f9c0963757e4d04d236340face82633ddc6739
mhmae <mhmae@unsitra.fr>
2025-06-03 09:52:06 +0200

    ♻️ Rename CreateProjectForm to NewProjectUsers, update related tests and imports & move Project requirements

---
+++ src/views/NewProjectView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n'
+import NewProjectRequirements from '@/components/newProject/newProjectRequirements/newProjectRequirements.vue'
+import NewProjectUsers from '@/components/newProject/newProjectUsers/NewProjectUsers.vue'
+
+const { t } = useI18n()
+</script>
+
+<template>
+    <main>
+        <h1>{{ t('newProject.creation.title') }}</h1>
+        <NewProjectRequirements />
+        <NewProjectUsers />
+    </main>
+</template>
