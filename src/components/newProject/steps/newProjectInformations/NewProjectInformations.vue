3318029f84c2ab446f21a9232f982cf1f68e40dc
mhmae <mhmae@unsitra.fr>
2025-06-05 09:56:59 +0200

    ♻️ Refactor NewProjectLibraries and NewProjectStepper to update component paths and integrate NewProjectInformations

---
+++ src/components/newProject/steps/newProjectInformations/NewProjectInformations.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useNewProjectStore } from '@/stores/newProjectStore.ts'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const { projectName, projectDescription } = storeToRefs(useNewProjectStore())
+</script>
+
+<template>
+    <QInput
+        v-model="projectName"
+        :label="t('newProject.steps.informations.name')"
+        type="text"
+    />
+    <QInput
+        v-model="projectDescription"
+        :label="t('newProject.steps.informations.description')"
+        type="textarea"
+    />
+</template>
+
+<style scoped></style>
