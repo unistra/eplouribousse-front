6ee1a534c4a4ae4192adc8c43592fd6a8d419947
mhmae <mhmae@unsitra.fr>
2025-07-10 11:46:16 +0200

    ✨💄 Refactor NewProjectRequirements and NewProjectStepper for improved layout and styling

--- src/components/newProject/newProjectRequirements/newProjectRequirements.vue
+++ src/components/newProject/newProjectRequirements/newProjectRequirements.vue
@@ -1,26 +1,19 @@
 <script lang="ts" setup>
-import { useUserStore } from '@/stores/userStore'
-import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import AtomicButton from '@/components/atomic/AtomicButton.vue'
 
 const { t } = useI18n()
-const userStore = useUserStore()
-const { tenantColor } = storeToRefs(userStore)
-const open = ref<boolean>(false)
-
 const emit = defineEmits<{
     (e: 'validate'): void
 }>()
 </script>
 
 <template>
-    <div class="col-8">
-        <p style="font-weight: bold">
+    <div class="requirements">
+        <p class="bold">
             {{ t('newProject.requirements.message') }}
         </p>
-        <ul class="margin-b1">
+        <ul>
             <li>{{ t('newProject.projectName') }}</li>
             <li>{{ t('newProject.pilotEmails') }}</li>
             <li>{{ t('newProject.adminEmails') }}</li>
@@ -30,7 +23,7 @@ const emit = defineEmits<{
             <li>{{ t('newProject.libraryInstructorEmails') }}</li>
             <li>
                 {{ t('newProject.libraryCollections') }}
-                <span style="font-weight: bold"
+                <span class="bold"
                     >(*) <QTooltip>{{ t('newProject.libraryResourceTooltip') }}</QTooltip></span
                 >
                 <ul>
@@ -42,48 +35,24 @@ const emit = defineEmits<{
             <li>{{ t('newProject.storageNames') }}</li>
             <li>{{ t('newProject.storageOperatorEmails') }}</li>
         </ul>
-        <!--        <QSeparator />-->
-        <!--        <p class="margin-t1 margin-b1">{{ t('newProject.searchUsers') }}</p>-->
-        <!--        <div class="container-center">-->
-        <!--            <QBtn-->
-        <!--                :label="t('newProject.requirements.invite')"-->
-        <!--                :style="tenantColor"-->
-        <!--                @click="open = true"-->
-        <!--            />-->
-        <!--        </div>-->
-        <!--        <SearchUser role="" />-->
+        <AtomicButton
+            color="primary"
+            :label="t('newProject.buttons.ready')"
+            no-border
+            @click="() => emit('validate')"
+        >
+            {{ t('newProject.requirements.requirementsOk') }}
+        </AtomicButton>
     </div>
-    <div class="col-3 table-center">
-        <div class="table-cell-vertical-align">
-            <AtomicButton
-                :label="t('newProject.buttons.ready')"
-                @click="() => emit('validate')"
-            >
-                {{ t('newProject.requirements.requirementsOk') }}
-            </AtomicButton>
-        </div>
-    </div>
-    <!--    <QDialog v-model="open">-->
-    <!--        <QCard-->
-    <!--            bordered-->
-    <!--            style="min-width: 24rem; min-height: 20rem"-->
-    <!--        >-->
-    <!--            <QCardSection style="display: flex; align-items: baseline">-->
-    <!--                <h6>{{ t('newProject.requirements.inviteUser') }}</h6>-->
-    <!--                <QSpace />-->
-    <!--                <QBtn-->
-    <!--                    v-close-popup-->
-    <!--                    dense-->
-    <!--                    flat-->
-    <!--                    icon="mdi-close"-->
-    <!--                    round-->
-    <!--                />-->
-    <!--            </QCardSection>-->
-
-    <!--            <QSeparator />-->
-    <!--            <QCardSection>-->
-    <!--                <InviteForm />-->
-    <!--            </QCardSection>-->
-    <!--        </QCard>-->
-    <!--    </QDialog>-->
 </template>
+
+<style lang="sass" scoped>
+.requirements
+    width: 100%
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+.bold
+    font-weight: bold
+</style>
