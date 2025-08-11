b7ed14709a3184ae5e04ec26a3c3a3b7fb0f8a3e
mhmae <mhmae@unsitra.fr>
2025-08-11 10:09:05 +0200

    ✨ Refactor axios interceptors for improved error handling and request configuration

--- src/plugins/axios/axios.ts
+++ src/plugins/axios/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, type AxiosResponse, type InternalAxiosRequestConfig } from 'axios'
+import axios, { AxiosError, type InternalAxiosRequestConfig } from 'axios'
 import { redirectTo403, isRouteAllowed, redirectionOrAddAuth } from './axiosUtils'
 
 const configAuth = {
@@ -11,27 +11,18 @@ const configAPI = {
 }
 const axiosI = axios.create(configAPI) // WITH INTERCEPTORS
 
-// Based on https://git.unistra.fr/vue-unistra/cas-authentication/-/blob/main/src/
-export const axiosRequestInterceptor = async (
-    config: InternalAxiosRequestConfig,
-): Promise<InternalAxiosRequestConfig> => {
-    if (isRouteAllowed('anon', config.url)) return config
+axiosI.interceptors.request.use(
+    async (config: InternalAxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
+        if (isRouteAllowed('anon', config.url)) return config
 
-    return await redirectionOrAddAuth(config, isRouteAllowed('anonAndAuth', config.url))
-}
-
-export const axios403Interceptor = async (response: AxiosError): Promise<void> => {
-    if (response.status === 403) {
-        await redirectTo403()
-    }
-}
-
-axiosI.interceptors.request.use(axiosRequestInterceptor, (error: unknown) => {
-    return Promise.reject(error)
-})
-
-axiosI.interceptors.response.use((response: AxiosResponse): AxiosResponse => {
-    return response
-}, axios403Interceptor)
+        return await redirectionOrAddAuth(config, isRouteAllowed('anonAndAuth', config.url))
+    },
+    async (error: AxiosError): Promise<never> => {
+        if (error.response?.status === 403) {
+            await redirectTo403()
+        }
+        return Promise.reject(error)
+    },
+)
 
 export { axiosI, axiosAuth }
