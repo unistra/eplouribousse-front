d00525d2afe78fc9d7fce7278c9e05914505bb3e
mhmae <mhmae@unsitra.fr>
2025-11-14 14:09:53 +0100

    ♻️ Refacto project requirements section to NewProjectView with localized messages
    di/eplouribousse/eplouribousse#154

--- src/views/NewProjectView.vue
+++ src/views/NewProjectView.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
 import ProjectStepper from '@/components/project/projectStepper/ProjectStepper.vue'
-import ProjectRequirements from '@/components/project/projectRequirements/ProjectRequirements.vue'
 import { ref } from 'vue'
 import { useProjectStore } from '@/stores/projectStore.ts'
+import AtomicButton from '@/components/atomic/AtomicButton.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 
 const isRequirementValidated = ref<boolean>(false)
 const store = useProjectStore()
@@ -11,10 +14,37 @@ store.$reset()
 
 <template>
     <QPage padding>
-        <ProjectRequirements
+        <div
             v-if="!isRequirementValidated"
-            @validate="isRequirementValidated = true"
-        />
+            class="requirements"
+        >
+            <div>
+                <h1>{{ t('view.project.new.requirements.heading') }}</h1>
+                <p>{{ t('view.project.new.requirements.message') }}</p>
+            </div>
+            <ul>
+                <li>{{ t('view.project.new.requirements.list.projectName') }}</li>
+                <li>{{ t('view.project.new.requirements.list.managerEmails') }}</li>
+                <li>{{ t('view.project.new.requirements.list.adminEmails') }}</li>
+                <li>{{ t('view.project.new.requirements.list.controllerEmail') }}</li>
+                <li>{{ t('view.project.new.requirements.list.libraryNames') }}</li>
+                <li>{{ t('view.project.new.requirements.list.libraryRCRs') }}</li>
+                <li>{{ t('view.project.new.requirements.list.libraryInstructorEmails') }}</li>
+                <li>
+                    {{ t('view.project.new.requirements.list.libraryCollections') }}
+                    <ul>
+                        <li>{{ t('view.project.new.requirements.list.collectionIdentified') }}</li>
+                        <li>{{ t('view.project.new.requirements.list.additionalInfos') }}</li>
+                    </ul>
+                </li>
+            </ul>
+            <AtomicButton
+                color="primary"
+                :label="t('newProject.buttons.ready')"
+                no-border
+                @click="isRequirementValidated = true"
+            />
+        </div>
         <ProjectStepper v-else />
     </QPage>
 </template>
@@ -22,4 +52,19 @@ store.$reset()
 <style lang="sass" scoped>
 .q-page
     display: flex
+
+    .requirements
+        width: 100%
+        display: flex
+        flex-direction: column
+        justify-content: center
+        align-items: center
+        gap: 1rem
+
+        div
+            display: flex
+            flex-direction: column
+            align-items: center
+        p
+            font-weight: bold
 </style>
