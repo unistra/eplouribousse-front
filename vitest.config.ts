a290f46fca631278ed71171a267f15c099c01ee3
mfreyder <mfreyder@unistra.fr>
2025-12-03 14:30:49 +0100

    🔧 Update Vite configuration to load environment variables and set default editor

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,17 +2,19 @@ import { fileURLToPath } from 'node:url'
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            environment: 'jsdom',
-            exclude: [...configDefaults.exclude, 'e2e/**'],
-            coverage: {
-                provider: 'v8',
-                reporter: ['text', 'json', 'html', 'lcov'],
+export default defineConfig((configEnv) => {
+    return mergeConfig(
+        viteConfig(configEnv),
+        defineConfig({
+            test: {
+                environment: 'jsdom',
+                exclude: [...configDefaults.exclude, 'e2e/**'],
+                coverage: {
+                    provider: 'v8',
+                    reporter: ['text', 'json', 'html', 'lcov'],
+                },
+                root: fileURLToPath(new URL('./', import.meta.url)),
             },
-            root: fileURLToPath(new URL('./', import.meta.url)),
-        },
-    }),
-)
+        }),
+    )
+})
