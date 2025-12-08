aeb5ac054071ab813245528f50f59e79d64295d4
mhmae <mhmae@unsitra.fr>
2025-12-08 13:35:33 +0100

    ♻️ Refactor declareAnomaly & validateControl on concerned composable

--- src/components/anomaly/AnomalyDeclarationBtn.vue
+++ src/components/anomaly/AnomalyDeclarationBtn.vue
@@ -2,13 +2,30 @@
 import AtomicButton from '@/components/atomic/AtomicButton.vue'
 import { useI18n } from 'vue-i18n'
 import { useResourceStore } from '@/stores/resourceStore.ts'
+import { useResourcesStore } from '@/stores/resourcesStore.ts'
+import { axiosI } from '@/plugins/axios/axios.ts'
+import type { Anomaly } from '#/project.ts'
+import { ResourceStatus } from '&/project.ts'
+import { useUtils } from '@/composables/useUtils.ts'
 
-const { t } = useI18n()
-const resourceStore = useResourceStore()
 const emit = defineEmits(['confirm'])
 
+const { t } = useI18n()
+const { useHandleError } = useUtils()
+const resourceStore = useResourceStore()
+
+const declareAnomaly = async () => {
+    const resourcesStore = useResourcesStore()
+    try {
+        await axiosI.patch<Anomaly>(`/resources/${resourceStore.resource?.id}/report-anomalies/`)
+        await resourcesStore.getResources({ status: [resourceStore.resource?.status || ResourceStatus.Positioning] })
+    } catch (e) {
+        useHandleError(e)
+    }
+}
+
 const onConfirm = async () => {
-    await resourceStore.declareAnomaly()
+    await declareAnomaly()
     emit('confirm')
 }
 </script>
