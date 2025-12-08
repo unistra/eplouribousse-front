76ea5f55fc39add279a8c5fdd1f47292b172bfc1
mhmae <mhmae@unsitra.fr>
2025-12-08 10:37:24 +0100

    🐛 Fix url generation to handle protocol + localhost

--- src/plugins/axios/axiosUtils.ts
+++ src/plugins/axios/axiosUtils.ts
@@ -2,9 +2,22 @@ import axios from 'axios'
 import { addJWTToLocalStorage, getJWT } from '@/utils/jwt.ts'
 
 const defineBackendBaseURL = () => {
-    const host = new URL(location.href).host.split('.')
-    const apiTenant = host.shift() + '-api.'
-    return apiTenant + host.join('.')
+    const url = new URL(location.href)
+    const hostParts = url.host.split('.')
+    if (hostParts.length === 1) return '' // URL should have subdomain, even in localhost (we don't throw error for test runner who runs on http://localhost:3000)
+
+    if (import.meta.env.DEV) {
+        // host is supposed to be tenant.epl.localhost:5173
+        hostParts[1] = hostParts[1] + '-api'
+        hostParts[2] = hostParts[2].replace(/\d+/, '8000')
+        url.host = hostParts.join('.')
+        return url.origin // tenant-api.epl.localhost:8000
+    }
+
+    // host is supposed to be tenant.eplouribousse.fr OR tenant.pprd.eplouribousse.fr
+    hostParts[0] = hostParts[0] + '-api'
+    url.host = hostParts.join('.')
+    return url.origin // tenant-api.eplouribousse.fr OR tenant-api.pprd.eplouribousse.fr
 }
 
 export const backendBaseURL = defineBackendBaseURL()
